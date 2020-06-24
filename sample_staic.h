7ddb1f4312b2826899e664ae3afd50bd713e5368
fazalellahi <faza.elahi@consoliads.com>
2020-06-24 19:07:54 +0500

    initial commit

---
+++ sample_staic.h
@@ -0,0 +1,15 @@
+//
+//  sample_staic.h
+//  sample-staic
+//
+//  Created by fazalWFH on 6/24/20.
+//  Copyright © 2020 ConsoliAds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sample_staic : NSObject
+
++(NSString*)getSDKVersion;
+
+@end
