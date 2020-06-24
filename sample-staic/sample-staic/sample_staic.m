cb6f351b16a246da33fcd467808cccd44491400b
fazalellahi <faza.elahi@consoliads.com>
2020-06-24 19:32:44 +0500

    added public header

---
+++ sample-staic/sample-staic/sample_staic.m
@@ -0,0 +1,13 @@
+//
+//  sample_staic.m
+//  sample-staic
+//
+//  Created by fazalWFH on 6/24/20.
+//  Copyright © 2020 ConsoliAds. All rights reserved.
+//
+
+#import "sample_staic.h"
+
+@implementation sample_staic
+
+@end
